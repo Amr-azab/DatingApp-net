9d7a146ef16a6f5242db69187c55885fc78280c6
Amr-Azab <20194736@fue.edu.eg>
2024-10-06 05:42:47 +0300

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,23 +15,24 @@ public class AccountController(DataContext context , ITokenService tokenService)
   [HttpPost("register")] //account/register
   public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
     if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-    using var hmac = new HMACSHA512();
-    var user = new AppUser 
-    {
-      UserName = registerDto.Username.ToLower(),
-      PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-      PasswordSalt=hmac.Key
+    return Ok();
+  //   using var hmac = new HMACSHA512();
+  //   var user = new AppUser 
+  //   {
+  //     UserName = registerDto.Username.ToLower(),
+  //     PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+  //     PasswordSalt=hmac.Key
 
-    };
-    context.Users.Add(user);
-    await context.SaveChangesAsync();
+  //   };
+  //   context.Users.Add(user);
+  //   await context.SaveChangesAsync();
 
-    return new UserDto
-    {
-      Username = user.UserName,
-      Token= tokenService.CreateToken(user)
-    };
-  }
+  //   return new UserDto
+  //   {
+  //     Username = user.UserName,
+  //     Token= tokenService.CreateToken(user)
+  //   };
+   }
   [HttpPost("login")]
   public async Task<ActionResult<UserDto>>Login(LoginDto loginDto){
     var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
