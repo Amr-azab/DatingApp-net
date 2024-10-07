084e4fb7c7839cac2c1b279307df5ac1af1bf0cd
Amr-Azab <20194736@fue.edu.eg>
2024-10-07 19:00:28 +0300

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 
 
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [Authorize]
-public class UsersController(IUserRepository userRepository ) : BaseApiController
+public class UsersController(IUserRepository userRepository ,IMapper mapper ) : BaseApiController
 {
   
   
@@ -29,5 +30,16 @@ public class UsersController(IUserRepository userRepository ) : BaseApiControlle
     if (user == null) return NotFound();
     return user;
 
+  }
+  [HttpPut]
+  public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+       var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+       if(username == null) return BadRequest("No username found is token ");
+       var user = await userRepository.GetUserByUsernameAsync(username);
+       if(user == null) return BadRequest("Could not find user");
+       mapper.Map(memberUpdateDto, user);
+       if(await userRepository.SaveAllAsync()) return NoContent();
+       return BadRequest("Faild to update the user ");
+       
   }
 }
