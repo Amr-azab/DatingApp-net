aceb4f4be2d3911ac633fba3b68c80efaeafebff
Amr-Azab <20194736@fue.edu.eg>
2024-10-09 20:52:01 +0300

    End of section 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -15,10 +16,18 @@ public class UsersController(IUserRepository userRepository ,IMapper mapper , IP
 {
   
   
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers(){
-    var users = await userRepository.GetMembersAsync();
+  // [HttpGet]
+  // public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers(){
+  //   var users = await userRepository.GetMembersAsync();
     
+  //   return Ok(users);
+
+  // }
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers([FromQuery]UserParams userParams){
+    userParams.CurrentUsername=User.GetUsername();
+    var users = await userRepository.GetMembersAsync(userParams);
+    Response.AddPaginationHeader(users);
     return Ok(users);
 
   }
